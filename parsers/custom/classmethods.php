c4056518b31c47644345b3fdd5e2023af34c7503
Ante Aljinovic <ante.aljinovic@socialbakers.com>
2013-04-12 10:12:03 +0200

    Added caching to classmethods parser
    * added caching to classmethods parser
    * added inheritance definition
    * added definition file position

--- parsers/custom/classmethods.php
+++ parsers/custom/classmethods.php
@@ -2,123 +2,138 @@
 
 class Kint_Parsers_ClassMethods extends kintParser
 {
+    private static $cache = array();
+
     protected function _parse(&$variable)
     {
         if(!is_object($variable)) {
             return false;
         }
 
-        $reflection = new \ReflectionClass($variable);
+        $className = get_class($variable);
 
-        $public = $private = $protected = array();
+        // Assuming class definition will not change inside one request
+        if(!isset(self::$cache[$className])) {
+            $reflection = new \ReflectionClass($variable);
 
-        // Class methods
-        foreach($reflection->getMethods() as $method) {
-            $params = array();
+            $public = $private = $protected = array();
 
-            // Access type
-            $access = implode(' ', \Reflection::getModifierNames($method->getModifiers()));
+            // Class methods
+            foreach($reflection->getMethods() as $method) {
+                $params = array();
 
-            // Method parameters
-            foreach($method->getParameters() as $param) {
-                $paramString = '';
+                // Access type
+                $access = implode(' ', \Reflection::getModifierNames($method->getModifiers()));
 
-                if($param->isArray()) {
-                    $paramString .= 'array ';
-                } elseif($param->getClass()) {
-                    $paramString .= $param->getClass()->name . ' ';
-                }
+                // Method parameters
+                foreach($method->getParameters() as $param) {
+                    $paramString = '';
 
-                $paramString .= ($param->isPassedByReference() ? '&' : '') . '$' . $param->getName();
+                    if($param->isArray()) {
+                        $paramString .= 'array ';
+                    } elseif($param->getClass()) {
+                        $paramString .= $param->getClass()->name . ' ';
+                    }
 
-                if($param->isDefaultValueAvailable()) {
-                    if(is_array($param->getDefaultValue())) {
-                        $arrayValues = array();
-                        foreach($param->getDefaultValue() as $key => $value) {
-                            $arrayValues[] = $key . ' => ' . $value;
+                    $paramString .= ($param->isPassedByReference() ? '&' : '') . '$' . $param->getName();
+
+                    if($param->isDefaultValueAvailable()) {
+                        if(is_array($param->getDefaultValue())) {
+                            $arrayValues = array();
+                            foreach($param->getDefaultValue() as $key => $value) {
+                                $arrayValues[] = $key . ' => ' . $value;
+                            }
+
+                            $defaultValue = 'array(' . implode(', ', $arrayValues) . ')';
+                        } elseif($param->getDefaultValue() === null){
+                            $defaultValue = 'NULL';
+                        } elseif($param->getDefaultValue() === false){
+                            $defaultValue = 'false';
+                        } elseif($param->getDefaultValue() === true){
+                            $defaultValue = 'true';
+                        } elseif($param->getDefaultValue() === ''){
+                            $defaultValue = '""';
+                        } else {
+                            $defaultValue = $param->getDefaultValue();
                         }
 
-                        $defaultValue = 'array(' . implode(', ', $arrayValues) . ')';
-                    } elseif($param->getDefaultValue() === null){
-                        $defaultValue = 'NULL';
-                    } elseif($param->getDefaultValue() === false){
-                        $defaultValue = 'false';
-                    } elseif($param->getDefaultValue() === true){
-                        $defaultValue = 'true';
-                    } elseif($param->getDefaultValue() === ''){
-                        $defaultValue = '""';
-                    } else {
-                        $defaultValue = $param->getDefaultValue();
+                        $paramString .= ' = ' . $defaultValue;
                     }
 
-                    $paramString .= ' = ' . $defaultValue;
+                    $params[] = $paramString;
                 }
 
-                $params[] = $paramString;
-            }
+                $output = new \kintVariableData();
 
-            $return = false;
-
-            // Simple DocBlock parser, look for @return
-            if(($docBlock = $method->getDocComment())) {
-                $matches = array();
-                if(preg_match_all('/@(\w+)\s+(.*)\r?\n/m', $docBlock, $matches)) {
-                    $lines = array_combine($matches[1], $matches[2]);
-                    if(isset($lines['return'])) {
-                        $return = $lines['return'];
+                // Simple DocBlock parser, look for @return
+                if(($docBlock = $method->getDocComment())) {
+                    $matches = array();
+                    if(preg_match_all('/@(\w+)\s+(.*)\r?\n/m', $docBlock, $matches)) {
+                        $lines = array_combine($matches[1], $matches[2]);
+                        if(isset($lines['return'])) {
+                            $output->operator = '->';
+                            $output->type = $lines['return'];
+                        }
                     }
                 }
-            }
 
-            $output = new \kintVariableData();
-            $output->name = ($method->returnsReference() ? '&' : '') . $method->getName() . '(' . implode(', ', $params) . ')';
-            $output->access = $access;
+                $output->name = ($method->returnsReference() ? '&' : '') . $method->getName() . '(' . implode(', ', $params) . ')';
+                $output->access = $access;
 
-            if(is_string($docBlock)) {
-                $lines = array();
-                foreach(explode("\n", $docBlock) as $line) {
-                    $line = trim($line);
+                if(is_string($docBlock)) {
+                    $lines = array();
+                    foreach(explode("\n", $docBlock) as $line) {
+                        $line = trim($line);
 
-                    if(in_array($line, array('/**', '/*', '*/'))) {
-                        continue;
-                    }elseif(strpos($line, '*') === 0) {
-                        $line = substr($line, 1);
+                        if(in_array($line, array('/**', '/*', '*/'))) {
+                            continue;
+                        }elseif(strpos($line, '*') === 0) {
+                            $line = substr($line, 1);
+                        }
+
+                        $lines[] = trim($line);
                     }
 
-                    $lines[] = trim($line);
+                    $output->extendedValue = implode("\n", $lines) . "\n\n";
                 }
 
-                $output->extendedValue = implode("\n", $lines);
+                $declaringClass = $method->getDeclaringClass();
+                $declaringClassName = $declaringClass->getName();
+
+                if($declaringClassName !== $className) {
+                    $output->extendedValue .= "<small>Inherited from <i>{$declaringClassName}</i></small>\n";
+                }
+
+                $fileName = \Kint::shortenPath($method->getFileName(), $method->getStartLine());
+
+                if($fileName) {
+                    $output->extendedValue .= "<small>Defined in {$fileName}</small>";
+                }
+
+                $sortName = $access . $method->getName();
+
+                if($method->isPrivate()) {
+                    $private[$sortName] = $output;
+                } elseif($method->isProtected()) {
+                    $protected[$sortName] = $output;
+                } else {
+                    $public[$sortName] = $output;
+                }
             }
 
-            if($return) {
-                $output->operator = '->';
-                $output->type = $return;
+            if(!$private && !$protected && !$public) {
+                self::$cache[$className] = false;
             }
 
-            $sortName = $access . $method->getName();
+            ksort($public);
+            ksort($protected);
+            ksort($private);
 
-            if($method->isPrivate()) {
-                $private[$sortName] = $output;
-            } elseif($method->isProtected()) {
-                $protected[$sortName] = $output;
-            } else {
-                $public[$sortName] = $output;
-            }
+            self::$cache[$className] = $public + $protected + $private;
         }
 
-        if(!$private && !$protected && !$public) {
-            return false;
-        }
-
-        ksort($public);
-        ksort($protected);
-        ksort($private);
-
-        $extendedValue = $public + $protected + $private;
-
-        $this->value = $extendedValue;
+        $this->value = self::$cache[$className];
         $this->type = 'methods';
-        $this->size = count($extendedValue);
+        $this->size = count(self::$cache[$className]);
     }
 }
