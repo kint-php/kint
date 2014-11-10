752afe5dfc4505ba635c0e7c048111346de60d5f
Rokas Šleinius <raveren@gmail.com>
2014-11-10 13:59:44 +0200

    fixes #135

--- parsers/custom/xml.php
+++ parsers/custom/xml.php
@@ -1,13 +1,18 @@
 <?php
+
 class Kint_Parsers_Xml extends kintParser
 {
 	protected function _parse( & $variable )
 	{
 		try {
-			if ( !is_string( $variable )
-				|| substr( $variable, 0, 5 ) !== '<?xml'
-				|| ( $xml = simplexml_load_string( $variable ) ) === null
-			) return false;
+			if ( is_string( $variable ) && substr( $variable, 0, 5 ) === '<?xml' ) {
+				$e   = libxml_use_internal_errors( true );
+				$xml = simplexml_load_string( $variable );
+				libxml_use_internal_errors( $e );
+				if ( empty( $xml ) ) {
+					return false;
+				}
+			}
 		} catch ( Exception $e ) {
 			return false;
 		}
