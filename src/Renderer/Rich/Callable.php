bd3e83251207ce99ff4491cc8a3d8f305fdfe760
Jonathan Vollebregt <jnvsor@gmail.com>
2017-01-14 16:58:27 +0100

    Kint_Renderer_Rich_Callable: Bugfix ValueShort str length limit

--- src/Renderer/Rich/Callable.php
+++ src/Renderer/Rich/Callable.php
@@ -21,13 +21,12 @@ class Kint_Renderer_Rich_Callable extends Kint_Renderer_Rich_Plugin
         }
 
         if (($s = $o->getValueShort()) !== null) {
-            if (Kint_Object_Blob::strlen($s) > Kint_Renderer_Rich::$strlen_max) {
+            if (Kint_Renderer_Rich::$strlen_max && Kint_Object_Blob::strlen($s) > Kint_Renderer_Rich::$strlen_max) {
                 $s = substr($s, 0, Kint_Renderer_Rich::$strlen_max).'...';
             }
             $header .= ' '.Kint_Object_Blob::escape($s);
         }
 
-        // $header .= Kint_Object_Blob::escape(Kint::shortenPath($o->filename)).':'.(int) $o->startline;
         $header = Kint_Renderer_Rich::renderHeaderWrapper($o, (bool) strlen($children), $header);
 
         return '<dl>'.$header.$children.'</dl>';
