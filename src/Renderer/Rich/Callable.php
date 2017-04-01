c8d0cce5b3c31816c9519d546c2345ac72afe6e0
Jonathan Vollebregt <jnvsor@gmail.com>
2017-04-01 08:46:30 +0200

    Kint_Renderer_Rich_Callable: Only cache methods
    Closures are too unpredictable and both their header and children
    can change based on where they are (Example - same closure in both
    an array and an object) so let's simplify and cache only methods.

--- src/Renderer/Rich/Callable.php
+++ src/Renderer/Rich/Callable.php
@@ -2,64 +2,55 @@
 
 class Kint_Renderer_Rich_Callable extends Kint_Renderer_Rich_Plugin
 {
-    protected static $cache = array(
-        'method' => array(),
-        'closure' => array(),
-    );
+    protected static $method_cache = array();
 
     public function render($o)
     {
-        if ($o instanceof Kint_Object_Method) {
-            if (!isset(self::$cache['method'][$o->owner_class])) {
-                self::$cache['method'][$o->owner_class] = array();
-            }
-            if (!isset(self::$cache['method'][$o->owner_class][$o->name])) {
-                self::$cache['method'][$o->owner_class][$o->name] = array();
-            }
-            $cache = &self::$cache['method'][$o->owner_class][$o->name];
-        } elseif ($o instanceof Kint_Object_Closure) {
-            if (!isset(self::$cache['closure'][$o->hash])) {
-                self::$cache['closure'][$o->hash] = array();
-            }
-            $cache = &self::$cache['closure'][$o->hash];
+        if ($o instanceof Kint_Object_Method && strlen($o->owner_class) && strlen($o->name) && !empty(self::$method_cache[$o->owner_class][$o->name])) {
+            $children = self::$method_cache[$o->owner_class][$o->name]['children'];
+
+            $header = $this->renderer->renderHeaderWrapper($o, (bool) strlen($children), self::$method_cache[$o->owner_class][$o->name]['header']);
+
+            return '<dl>'.$header.$children.'</dl>';
         }
 
-        if ($o instanceof Kint_Object_Method) {
-            if (empty($cache)) {
-                $cache['children'] = $this->renderer->renderChildren($o);
-            }
+        $children = $this->renderer->renderChildren($o);
 
-            $children = $cache['children'];
-        } else {
-            $children = $this->renderer->renderChildren($o);
+        $header = '';
+
+        if (($s = $o->getModifiers()) !== null) {
+            $header .= '<var>'.$s.'</var> ';
         }
 
-        if (empty($cache['header'])) {
-            $header = '';
-
-            if (($s = $o->getModifiers()) !== null) {
-                $header .= '<var>'.$s.'</var> ';
-            }
-
-            if (($s = $o->getName()) !== null) {
-                $header .= '<dfn>'.Kint_Object_Blob::escape($s).'('.Kint_Object_Blob::escape($o->getParams()).')</dfn>';
-            }
-
-            if (!empty($o->returntype)) {
-                $header .= ': <var>'.Kint_Object_Blob::escape($o->returntype).'</var>';
-            }
-
-            if (($s = $o->getValueShort()) !== null) {
-                if (Kint_Renderer_Rich::$strlen_max && Kint_Object_Blob::strlen($s) > Kint_Renderer_Rich::$strlen_max) {
-                    $s = substr($s, 0, Kint_Renderer_Rich::$strlen_max).'...';
-                }
-                $header .= ' '.Kint_Object_Blob::escape($s);
-            }
-
-            $cache['header'] = $header;
+        if (($s = $o->getName()) !== null) {
+            $header .= '<dfn>'.Kint_Object_Blob::escape($s).'('.Kint_Object_Blob::escape($o->getParams()).')</dfn>';
         }
 
-        $header = $this->renderer->renderHeaderWrapper($o, (bool) strlen($children), $cache['header']);
+        if (!empty($o->returntype)) {
+            $header .= ': <var>'.Kint_Object_Blob::escape($o->returntype).'</var>';
+        }
+
+        if (($s = $o->getValueShort()) !== null) {
+            if (Kint_Renderer_Rich::$strlen_max && Kint_Object_Blob::strlen($s) > Kint_Renderer_Rich::$strlen_max) {
+                $s = substr($s, 0, Kint_Renderer_Rich::$strlen_max).'...';
+            }
+            $header .= ' '.Kint_Object_Blob::escape($s);
+        }
+
+        if ($o instanceof Kint_Object_Method && strlen($o->owner_class) && strlen($o->name)) {
+            $cache = array(
+                'header' => $header,
+                'children' => $children,
+            );
+
+            if (!isset(self::$method_cache[$o->owner_class])) {
+                self::$method_cache[$o->owner_class] = array($o->name => $cache);
+            } elseif (!isset(self::$method_cache[$o->owner_class][$o->name])) {
+                self::$method_cache[$o->owner_class][$o->name] = $cache;
+            }
+        }
+
+        $header = $this->renderer->renderHeaderWrapper($o, (bool) strlen($children), $header);
 
         return '<dl>'.$header.$children.'</dl>';
     }
