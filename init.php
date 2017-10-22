3a393ddd3516632969c7dcb03195f285a6770313
Jonathan Vollebregt <jnvsor@gmail.com>
2017-10-22 19:21:41 +0200

    Use __DIR__ instead of dirname(__FILE__)

--- init.php
+++ init.php
@@ -8,12 +8,12 @@ if (defined('KINT_DIR')) {
     return;
 }
 
-require_once dirname(__FILE__).'/init_header.php';
+require_once __DIR__.'/init_header.php';
 
 // Check composer for extras disabling default helper functions
 if (!Kint::composerGetDisableHelperFunctions()) {
-    require_once KINT_DIR.'/init_helpers.php';
+    require_once __DIR__.'/init_helpers.php';
 }
 
 // Dynamic default settings
-require_once KINT_DIR.'/init_footer.php';
+require_once __DIR__.'/init_footer.php';
--- init_header.php
+++ init_header.php
@@ -29,7 +29,7 @@ if (version_compare(PHP_VERSION, '5.3') < 0) {
     throw new Exception('Kint 3.0 requires PHP 5.3 or higher');
 }
 
-define('KINT_DIR', dirname(__FILE__));
+define('KINT_DIR', __DIR__);
 define('KINT_WIN', DIRECTORY_SEPARATOR !== '/');
 define('KINT_PHP56', (version_compare(PHP_VERSION, '5.6') >= 0));
 define('KINT_PHP70', (version_compare(PHP_VERSION, '7.0') >= 0));
