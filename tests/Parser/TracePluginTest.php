85bb025979c7e117b4e9b4fc79b2ad154664eafb
Jonathan Vollebregt <jnvsor@gmail.com>
2017-12-30 14:46:05 +0100

    TracePluginTest: 100% coverage

--- tests/Parser/TracePluginTest.php
+++ tests/Parser/TracePluginTest.php
@@ -9,6 +9,8 @@ use PHPUnit_Framework_TestCase;
 
 class TracePluginTest extends PHPUnit_Framework_TestCase
 {
+    protected $blacklist_stash;
+
     /**
      * @covers \Kint\Parser\TracePlugin::parse
      */
@@ -28,13 +30,79 @@ class TracePluginTest extends PHPUnit_Framework_TestCase
         $this->assertInstanceOf('Kint\\Object\\TraceFrameObject', $o->value->contents[0]);
     }
 
+    /**
+     * @covers \Kint\Parser\TracePlugin::parse
+     */
+    public function testParseMismatch()
+    {
+        $bt = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+        $b = BasicObject::blank();
+        $parser = new Parser();
+        $plugin = new TracePlugin();
+
+        $incorrect = $parser->parse($bt, clone $b);
+        $incorrect->value->contents[0]->name = 'newName';
+        $parser->addPlugin($plugin);
+        $plugin->parse($bt, $incorrect, Parser::TRIGGER_SUCCESS);
+
+        array_shift($bt);
+        $correct = $parser->parse($bt, clone $b);
+
+        foreach ($correct->value->contents as $frame) {
+            ++$frame->name;
+        }
+
+        $this->assertEquals($correct, $incorrect);
+    }
+
+    /**
+     * @covers \Kint\Parser\TracePlugin::parse
+     */
+    public function testParseNoValue()
+    {
+        $p = new TracePlugin();
+
+        $b = BasicObject::blank();
+        $o = clone $b;
+        $v = array();
+
+        $p->parse($v, $o, Parser::TRIGGER_SUCCESS);
+
+        $this->assertEquals($b, $o);
+    }
+
+    /**
+     * @covers \Kint\Parser\TracePlugin::parse
+     */
+    public function testParseBlacklist()
+    {
+        $bt = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+        $shortbt = $bt;
+        array_shift($shortbt);
+
+        $p = new Parser();
+        $p->addPlugin(new TracePlugin());
+
+        $b = BasicObject::blank();
+
+        $o = $p->parse($shortbt, clone $b);
+
+        foreach ($o->value->contents as $frame) {
+            ++$frame->name;
+        }
+
+        TracePlugin::$blacklist[] = array(__CLASS__, __FUNCTION__);
+
+        $this->assertEquals($o->value, $p->parse($bt, clone $b)->value);
+    }
+
     public function provideTraces()
     {
         $bt = debug_backtrace(true);
         $bad_bt_1 = $bt;
         $bad_bt_1[0]['test'] = 'woot';
         $bad_bt_2 = $bt;
-        $bat_bt_2[0]['function'] = 1234;
+        $bad_bt_2[0]['function'] = 1234;
 
         return array(
             'empty' => array(
@@ -50,7 +118,7 @@ class TracePluginTest extends PHPUnit_Framework_TestCase
                 'expect' => false,
             ),
             'bad backtrace, wrong type' => array(
-                'trace' => $bad_bt_1,
+                'trace' => $bad_bt_2,
                 'expect' => false,
             ),
             'mythical' => array(
@@ -62,6 +130,10 @@ class TracePluginTest extends PHPUnit_Framework_TestCase
                     ),
                 ),
                 'expect' => true,
+            ),
+            'normal array' => array(
+                'trace' => array(1, 2, 3),
+                'expect' => false,
             ),
         );
     }
@@ -153,5 +225,27 @@ class TracePluginTest extends PHPUnit_Framework_TestCase
         TracePlugin::normalizeAliases($input);
 
         $this->assertEquals($expected, $input);
+    }
+
+    /**
+     * @covers \Kint\Parser\TracePlugin::getTypes
+     * @covers \Kint\Parser\TracePlugin::getTriggers
+     */
+    public function testHooks()
+    {
+        $p = new TracePlugin();
+
+        $this->assertEquals(array('array'), $p->getTypes());
+        $this->assertEquals(Parser::TRIGGER_SUCCESS, $p->getTriggers());
+    }
+
+    public function setUp()
+    {
+        $this->blacklist_stash = TracePlugin::$blacklist;
+    }
+
+    public function tearDown()
+    {
+        TracePlugin::$blacklist = $this->blacklist_stash;
     }
 }
