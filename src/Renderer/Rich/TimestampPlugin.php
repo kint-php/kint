dd7d7ca9136dcc04b90f85dc3d4bd1d57a537b22
Jonathan Vollebregt <jnvsor@gmail.com>
2018-04-13 19:45:38 +0200

    TimestampPlugin: Always use UTC

--- src/Renderer/Rich/TimestampPlugin.php
+++ src/Renderer/Rich/TimestampPlugin.php
@@ -2,13 +2,17 @@
 
 namespace Kint\Renderer\Rich;
 
+use DateTime;
+use DateTimeZone;
 use Kint\Object\Representation\Representation;
 
 class TimestampPlugin extends Plugin implements TabPluginInterface
 {
     public function renderTab(Representation $r)
     {
-        // Avoid dreaded "Timezone must be set" error
-        return '<pre>'.@date('Y-m-d H:i:s', $r->contents).'</pre>';
+        return '<pre>'.
+            DateTime::createFromFormat('U', $r->contents)
+                ->setTimeZone(new DateTimeZone('UTC'))
+                ->format('Y-m-d H:i:s T').'</pre>';
     }
 }
