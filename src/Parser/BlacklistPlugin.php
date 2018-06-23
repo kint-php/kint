27ac8a9292fc17240dad468cfa24a001eed1a875
Jonathan Vollebregt <jnvsor@gmail.com>
2018-06-23 16:30:49 +0200

    BlacklistPlugin: Support blacklisting arrays by size

--- src/Parser/BlacklistPlugin.php
+++ src/Parser/BlacklistPlugin.php
@@ -44,9 +44,23 @@ class BlacklistPlugin extends Plugin
      */
     public static $shallow_blacklist = array();
 
+    /**
+     * Maximum size of arrays before blacklisting.
+     *
+     * @var int
+     */
+    public static $array_limit = 10000;
+
+    /**
+     * Maximum size of arrays before blacklisting except when dumped directly.
+     *
+     * @var int
+     */
+    public static $shallow_array_limit = 1000;
+
     public function getTypes()
     {
-        return array('object');
+        return array('object', 'array');
     }
 
     public function getTriggers()
@@ -56,9 +70,19 @@ class BlacklistPlugin extends Plugin
 
     public function parse(&$var, BasicObject &$o, $trigger)
     {
+        if (\is_object($var)) {
+            return $this->parseObject($var, $o);
+        }
+        if (\is_array($var)) {
+            return $this->parseArray($var, $o);
+        }
+    }
+
+    protected function parseObject(&$var, BasicObject &$o)
+    {
         foreach (self::$blacklist as $class) {
             if ($var instanceof $class) {
-                return $this->blacklist($var, $o);
+                return $this->blacklistObject($var, $o);
             }
         }
 
@@ -68,12 +92,12 @@ class BlacklistPlugin extends Plugin
 
         foreach (self::$shallow_blacklist as $class) {
             if ($var instanceof $class) {
-                return $this->blacklist($var, $o);
+                return $this->blacklistObject($var, $o);
             }
         }
     }
 
-    protected function blacklist(&$var, &$o)
+    protected function blacklistObject(&$var, BasicObject &$o)
     {
         $object = new InstanceObject();
         $object->transplant($o);
@@ -82,6 +106,34 @@ class BlacklistPlugin extends Plugin
         $object->clearRepresentations();
         $object->value = null;
         $object->size = null;
+        $object->hints[] = 'blacklist';
+
+        $o = $object;
+
+        $this->parser->haltParse();
+    }
+
+    protected function parseArray(array &$var, BasicObject &$o)
+    {
+        if (\count($var) > self::$array_limit) {
+            return $this->blacklistArray($var, $o);
+        }
+
+        if ($o->depth <= 0) {
+            return;
+        }
+
+        if (\count($var) > self::$shallow_array_limit) {
+            return $this->blacklistArray($var, $o);
+        }
+    }
+
+    protected function blacklistArray(array &$var, BasicObject &$o)
+    {
+        $object = new BasicObject();
+        $object->transplant($o);
+        $object->value = null;
+        $object->size = \count($var);
         $object->hints[] = 'blacklist';
 
         $o = $object;
