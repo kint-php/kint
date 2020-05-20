669292fdcbceb4dea39727ca6ffc72e3dce65a35
Jonathan Vollebregt <jnvsor@gmail.com>
2020-05-20 20:30:08 +0200

    init: Change warning to min php version 5.6

--- init.php
+++ init.php
@@ -30,8 +30,8 @@ if (\defined('KINT_DIR')) {
     return;
 }
 
-if (\version_compare(PHP_VERSION, '5.3') < 0) {
-    throw new Exception('Kint 3.0 requires PHP 5.3 or higher');
+if (\version_compare(PHP_VERSION, '5.6') < 0) {
+    throw new Exception('Kint 4.0 requires PHP 5.6 or higher');
 }
 
 \define('KINT_DIR', __DIR__);
